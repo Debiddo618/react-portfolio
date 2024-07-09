701acfe108d21cc24ab7c74aa37d3e6668d5eb27
Debiddo <dzheng@terpmail.umd.edu>
2024-07-09 12:12:56 -0400

    added animation to hero section

--- react-portfolio/src/components/Experience/Experience.jsx
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 
+import { useRef, useState, useEffect } from "react";
 import styles from "./Experience.module.css";
 import skills from "../../data/skills.json";
 import history from "../../data/history.json";
 import { getImageUrl } from "../../utils";
 
 export const Experience = () => {
+  const [visible, setVisible] = useState([]);
+  const historyRefs = useRef([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const index = historyRefs.current.indexOf(entry.target);
+          if (index !== -1) {
+            setVisible((prev) => {
+              const updated = [...prev];
+              updated[index] = true;
+              return updated;
+            });
+          }
+        }
+      });
+    });
+
+    historyRefs.current.forEach((ref) => {
+      if (ref) {
+        observer.observe(ref);
+      }
+    });
+
+  }, []);
+
   return (
     <section className={styles.container} id="experience">
       <h2 className={styles.title}>Experience & Education</h2>
@@ -14,7 +42,7 @@ export const Experience = () => {
           {skills.map((skill, id) => {
             return (
               <div key={id} className={styles.skill}>
-                <div className={styles.skillImageContainer}>
+                <div className={styles.skillImageContainer} style={{ animationDelay: `${id * 0.5}s` }}>
                   <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
                 </div>
                 <p>{skill.title}</p>
@@ -25,7 +53,7 @@ export const Experience = () => {
         <ul className={styles.history}>
           {history.map((historyItem, id) => {
             return (
-              <li key={id} className={styles.historyItem}>
+              <li key={id} className={styles.historyItem} ref={(el) => (historyRefs.current[id] = el)}>
                 <img
                   src={getImageUrl(historyItem.imageSrc)}
                   alt={`${historyItem.organisation} Logo`}
