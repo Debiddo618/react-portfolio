270745af5cdfbb89f8cbc025256fb5d4752d61fa
Debiddo <dzheng@terpmail.umd.edu>
2024-07-15 00:16:06 -0400

    removed animation on contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -14,7 +14,7 @@ export const Contact = () => {
   }, [])
 
   return (
-    <footer id="contact" className={styles.container} data-aos="flip-up">
+    <footer id="contact" className={styles.container}>
       <div className={styles.text}>
         <h2>Contact</h2>
         <p>Feel free to reach out!</p>
